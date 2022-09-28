2011a816708ebdfa83f92e4ca7cd2fc84a64cd24
Dino <dino15031992@gmail.com>
2022-09-28 14:35:05 +0200

    Initial commit

---
+++ src/PrintYourNameAndSurnameAndPi.java
@@ -0,0 +1,13 @@
+public class PrintYourNameAndSurnameAndPi {
+    public static void main(String[] args){
+
+        String name = "dino";
+        double pi = 3.141590;
+        // variant
+        String pi2 = "3.141590";
+        System.out.printf(" %S %n ", name);
+        System.out.printf(" The number Pi is %f %n ", pi);
+        System.out.printf("  Variant: %s ",pi2);
+
+    }
+}
